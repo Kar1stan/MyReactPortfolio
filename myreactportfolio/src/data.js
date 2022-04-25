06253c90d86f293de1753cba1c60d311f46beb21
Kar1stan <90850632+Kar1stan@users.noreply.github.com>
2022-04-25 16:30:11 +0300

    Add files via upload

---
+++ myreactportfolio/src/data.js
@@ -0,0 +1,31 @@
+import PortfolioScreen1 from "./assets/PortfolioScreen1.PNG";
+import PortfolioScreen2 from "./assets/PortfolioScreen2.PNG";
+import PortfolioScreen3 from "./assets/PortfolioScreen3.PNG";
+
+export const projects = [
+    {
+      title: "ReactSite1",
+      subtitle: "React",
+      description:
+        "Web-site  using React theme is cosmos.",
+      image: PortfolioScreen1,
+      link: "https://github.com/Kar1stan/React_Projects.git",
+    },
+    {
+      title: "Angular Currency",
+      subtitle: "Angular",
+      description:
+        "Currency conventor using Angular(UAH,USD,EUR).",
+      image:PortfolioScreen2,
+      link: "https://github.com/Kar1stan/AngularCurrency.git",
+    },
+    {
+      title: "React Streets",
+      subtitle: "React",
+      description:
+        "Web-app which show popular streets using React",
+      image:PortfolioScreen3,
+      link: "https://github.com/Kar1stan/React_Projects.git",
+    },
+  ];
+  export const skills= ["English Upper Intermediate"];
