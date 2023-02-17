0ec0f79921149a1436c283e5c5a20daac05f39ed
Karastan Vladislav <90850632+Kar1stan@users.noreply.github.com>
2023-02-17 14:54:32 +0300

    Add files via upload

---
+++ src/data.js
@@ -0,0 +1,49 @@
+import PortfolioScreen1 from "./assets/PortfolioScreen1.PNG";
+import PortfolioScreen2 from "./assets/PortfolioScreen2.PNG";
+import PortfolioScreen3 from "./assets/PortfolioScreen3.PNG";
+import PortfolioScreen4 from "./assets/PortfolioScreen4.png";
+import PortfolioScreen5 from "./assets/PortfolioScreen5.png";
+
+export const projects = [
+    {
+      title: "ReactCosmosSite",
+      subtitle: "React",
+      description:
+        "Simple site using React.",
+      image: PortfolioScreen1,
+      link: "https://github.com/Kar1stan/ReactCosmosSite.git",
+    },
+    {
+      title: "Angular Currency",
+      subtitle: "Angular",
+      description:
+        "Currency conventor using Angular(UAH,USD,EUR).",
+      image:PortfolioScreen2,
+      link: "https://github.com/Kar1stan/AngularCurrency.git",
+    },
+    {
+      title: "React Streets",
+      subtitle: "React",
+      description:
+        "Web-app which show popular streets using React",
+      image:PortfolioScreen3,
+      link: "https:https://github.com/Kar1stan/ReactStreets.git",
+    },
+    {
+      title: "ReactVideoPlayer",
+      subtitle: "React",
+      description:
+        "Video player on React consists of 4 videos",
+      image:PortfolioScreen4,
+      link: "https://github.com/Kar1stan/ReactVideoPlayer.git",
+    },
+    {
+      title: "ReactBMICalculator",
+      subtitle: "React",
+      description:
+        "BMI (body mass index) calculator on React",
+      image:PortfolioScreen5,
+      link: "https://github.com/Kar1stan/ReactBMICalculator.git",
+    },
+  ];
+  export const skills= ["English Upper Intermediate","Git","Docker"];
